7791e08666d18d67063eec93f076bfac9ffc1b5d
Parker Lusk <parkerclusk@gmail.com>
2018-12-11 14:24:56 -0500

    I think .. it's working? That can't be right...

--- feature_tracker/src/attention_viewer_ros.cpp
+++ feature_tracker/src/attention_viewer_ros.cpp
@@ -46,6 +46,9 @@ void AttentionViewerROS::callback(const sensor_msgs::ImageConstPtr& _img,
                                   const sensor_msgs::PointCloudConstPtr& _features,
                                   const sensor_msgs::PointCloudConstPtr& _selinfo)
 {
+  // bail if no one wants to listen to us
+  if (img_pub_.getNumSubscribers() == 0) return;
+
   cv::Mat img;
 
   try {
@@ -55,80 +58,47 @@ void AttentionViewerROS::callback(const sensor_msgs::ImageConstPtr& _img,
     return;
   }
 
-  //
-  // Publish ROS measurements
-  //
+  // for convenience
+  const auto& oldIds = _selinfo->channels[0].values;
+  const auto& newIds = _selinfo->channels[1].values;
 
-  // sensor_msgs::PointCloudPtr cloud(new sensor_msgs::PointCloud);
-  // sensor_msgs::ChannelFloat32 ids;
-  // sensor_msgs::ChannelFloat32 pts_u;
-  // sensor_msgs::ChannelFloat32 pts_v;
-  // sensor_msgs::ChannelFloat32 vel_x;
-  // sensor_msgs::ChannelFloat32 vel_y;
+  for (size_t i=0; i<_features->points.size(); ++i) {
+    // auto nip = cv::Point2f{_features->points[i].x, _features->points[i].y};
+    int id = static_cast<int>(_features->channels[0].values[i]);
+    auto pix = cv::Point2f(_features->channels[1].values[i], _features->channels[2].values[i]);
+    auto vel = cv::Point2f(_features->channels[3].values[i], _features->channels[4].values[i]);
 
-  // cloud->header = msg->header;
-  // // cloud->header.frame_id = "world";
+    bool isOld = std::find(oldIds.begin(), oldIds.end(), id) != oldIds.end();
+    bool isNew = std::find(newIds.begin(), newIds.end(), id) != newIds.end();
 
-  // for (size_t i=0; i<measurements.size(); ++i) {
-  //   auto id = std::get<0>(measurements[i]);
-  //   auto pt = std::get<1>(measurements[i]);
-  //   auto nip = std::get<2>(measurements[i]);
-  //   auto ell = std::get<3>(measurements[i]);
-  //   auto vel = std::get<4>(measurements[i]);
+    // this feature was never chosen
+    if (!isOld && !isNew) continue;
 
-  //   geometry_msgs::Point32 cloud_pt;
-  //   cloud_pt.x = nip.x;
-  //   cloud_pt.y = nip.y;
-  //   cloud_pt.z = 1.0f;
+    // new features are blue, old features red
+    double alpha = (isNew) ? 0 : ((isOld) ? 1 : 0.5);
+    auto color = cv::Scalar(255 * (1 - alpha), 0, 255 * alpha);
 
-  //   cloud->points.push_back(cloud_pt);
-  //   ids.values.push_back(id);
-  //   pts_u.values.push_back(pt.x);
-  //   pts_v.values.push_back(pt.y);
-  //   vel_x.values.push_back(vel.x);
-  //   vel_y.values.push_back(vel.y);
-  // }
+    // draw feature
+    cv::circle(img, pix, 2, color, 2);
 
-  // cloud->channels.push_back(ids);
-  // cloud->channels.push_back(pts_u);
-  // cloud->channels.push_back(pts_v);
-  // cloud->channels.push_back(vel_x);
-  // cloud->channels.push_back(vel_y);
+    // // draw velocity
+    // constexpr double dt = 0.10;
+    // cv::Point2f nip0 = nip - dt*vel;
+    // Eigen::Vector3d b0(nip0.x, nip0.y, 1.0);
+    // Eigen::Vector2d a0;
+    // tracker_->camera()->spaceToPlane(b0, a0);
+    // cv::line(img, pix, cv::Point2f(a0.x(), a0.y()), cv::Scalar(255 , 0, 0), 1 , 8, 0);
 
-  // features_pub_.publish(cloud);
-
-  // optional feature visualization publish
-  if (img_pub_.getNumSubscribers()) {
-    for (size_t i=0; i<_features->points.size(); ++i) {
-      // auto nip = cv::Point2f{_features->points[i].x, _features->points[i].y};
-      int id = static_cast<int>(_features->channels[0].values[i]);
-      auto pix = cv::Point2f(_features->channels[1].values[i], _features->channels[2].values[i]);
-      auto vel = cv::Point2f(_features->channels[3].values[i], _features->channels[4].values[i]);
-
-      auto color = cv::Scalar(255, 0, 255);
-
-      // draw feature
-      cv::circle(img, pix, 2, color, 2);
-
-      // // draw velocity
-      // constexpr double dt = 0.10;
-      // cv::Point2f nip0 = nip - dt*vel;
-      // Eigen::Vector3d b0(nip0.x, nip0.y, 1.0);
-      // Eigen::Vector2d a0;
-      // tracker_->camera()->spaceToPlane(b0, a0);
-      // cv::line(img, pix, cv::Point2f(a0.x(), a0.y()), cv::Scalar(255 , 0, 0), 1 , 8, 0);
-
-      // print id next to feature
-      char strID[10];
-      sprintf(strID, "%d", id);
-      cv::putText(img, strID, pix, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-    }
-
-    // pack up and publish
-    cv_bridge::CvImage out;
-    out.header = _img->header;
-    out.encoding = sensor_msgs::image_encodings::BGR8;
-    out.image = img;
-    img_pub_.publish(out.toImageMsg());
+    // print id next to feature
+    char strID[10];
+    sprintf(strID, "%d", id);
+    cv::putText(img, strID, pix, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
   }
+
+  // pack up and publish
+  cv_bridge::CvImage out;
+  out.header = _img->header;
+  out.encoding = sensor_msgs::image_encodings::BGR8;
+  out.image = img;
+  img_pub_.publish(out.toImageMsg());
 }
